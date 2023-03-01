41b5dffc14261eea6570c69fe14f3a1ba1b7da67
Debasish Bhola <devop69@gmail.com>
2023-03-01 20:08:57 +0530

    Feat

--- app/src/main/java/com/example/sounddemo/MainActivity.java
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer mplayer;
 
     public void playAudio(View view){
-        mplayer = MediaPlayer.create(this, R.raw.music);
 
         mplayer.start();
     }
@@ -25,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        mplayer = MediaPlayer.create(this, R.raw.music);
     }
 }
