6ac23c28e2908752229b93c563937181d9f69aa7
Debasish Bhola <devop69@gmail.com>
2023-03-01 20:06:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.sounddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mplayer;
+
+    public void playAudio(View view){
+        mplayer = MediaPlayer.create(this, R.raw.music);
+
+        mplayer.start();
+    }
+    public void pauseAudio(View view){
+
+        mplayer.stop();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
